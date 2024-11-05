c8bdd2b8d4fefa0b6383c9a48974cbb41d735eec
Yatz <Jamessandayan17@gmail.com>
2024-11-05 13:25:43 +0800

    Update IT5

---
+++ IT5/activity1.py
@@ -0,0 +1,7 @@
+grocery_list = ['milk', 'bread', 'eggs', 'apples', 'cheese']
+
+grocery_list.append('butter')
+grocery_list.append('orange juice')
+grocery_list.remove('bread')
+print("First item:", grocery_list[0])
+print("Last item:", grocery_list[-1])
