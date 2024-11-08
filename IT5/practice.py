0574ade0776911488216f0114d1a6af87c779c84
Yatz <Jamessandayan17@gmail.com>
2024-11-08 13:22:49 +0800

    Create practice.py

---
+++ IT5/practice.py
@@ -0,0 +1,9 @@
+word = " Hello, World "
+print(word.upper())
+print(word.lower())
+print(word.strip())
+print(word.replace("World", "Python"))
+print(word.split(","))
+print("-".join(["A", "B", "C"]))
+print(word.find("World"))
+print(word.count("l"))
